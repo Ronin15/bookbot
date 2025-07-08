bcba7fd62bfb1b9f004905203b14e24a4cc7c316
RoninXV <simsjohn15@gmail.com>
2025-07-08 16:55:20 -0700

    project update

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text():
+    with open("books/frankenstein.txt") as f:
+        # Read the entire content of the file
+        return f.read()
+
+def main():
+    text = get_book_text()
+    print(text)
+
+if __name__ == "__main__":
+    main()
