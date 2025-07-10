d11dccc3a744b3db9283753cbea89bc202198a8c
RoninXV <simsjohn15@gmail.com>
2025-07-09 20:11:33 -0700

    update

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 
-from stats import get_book_text, count_characters
+from stats import get_book_text, count_characters, sort_character_count
 
 def main():
     text = get_book_text()
     num_words = len(text.split())
-    print(f"{num_words} words found in the document.")
     char_counts = count_characters(text)
-    print(char_counts)
+    sorted_chars = sort_character_count(char_counts)
+    
+    print("================ BOOKBOT========================\n")
+    print("Analyzing book found at books/frankenstein.txt.\n")
+    print(f"--------------- Word count ---------------------\n")
+    print(f"Found {num_words} total words\n")
+    print("--------------- Character count -----------------\n")
+    for char_data in sorted_chars:
+        print(f"{char_data['char']}: {char_data['num']}\n")
+    
+    print("================= END =========================\n")
 
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -6,8 +6,16 @@ def get_book_text():
 def count_characters(text):
     char_counts = {}
     for char in text.lower():
-        if char in char_counts:
-            char_counts[char] += 1
-        else:
-            char_counts[char] = 1
+        if char.isalpha():
+            if char in char_counts:
+                char_counts[char] += 1
+            else:
+                char_counts[char] = 1
     return char_counts
+
+def sort_character_count(char_counts):
+    sorted_chars = []
+    for char, count in char_counts.items():
+        sorted_chars.append({"char": char, "num": count})
+    sorted_chars.sort(key=lambda x: x["num"], reverse=True)
+    return sorted_chars
